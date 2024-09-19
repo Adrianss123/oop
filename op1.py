5dd1ff3b4bd02289f3ba94d6a4c6784df9efcdc2
Darja1 <91080197+Darja1@users.noreply.github.com>
2024-09-19 14:14:44 +0300

    Create op1.py

---
+++ op1.py
@@ -0,0 +1,58 @@
+class person:
+    def set(self,name,age):
+        self.name = name
+        self.age = age
+    def output(self):
+            print(f"Persona {self.name}, kuram ir {self.age} gadi") 
+
+p = person()
+p.set("Peter",20)
+p.output()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
