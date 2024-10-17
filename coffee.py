dfe51f24a5082d86758e9363d005b32d2ecedc3a
Adrianss123 <taskansadrians93@gmail.com>
2024-10-17 14:25:09 +0300

    s\casdfsdf

---
+++ coffee.py
@@ -0,0 +1,16 @@
+class coffee:
+ 
+ def __init__(self,marka,uznemums):
+  self.marka=marka
+  self.uznemums=uznemums
+
+
+
+ def pagatavot_coffee(self, coffee_veids):
+   print(f"izvelies kafijas marku{coffee_veids} using {self.marka}")
+kafija1=
+
+ def info(self):
+  print(f"coffee  marka:{self.marka}")
+  print(f"uznemums no:{self.uznemums}")
+
--- rect.py
+++ rect.py
@@ -1,18 +1,48 @@
-from random import*
+import random 
+
+
 class Rectangle:
-    def __init__(self,name,width=0,height=0,color='green'):
+    count=0
+    def __init__(self,name,width=10,height=20,color='blue'):
+    
         self.width=width
         self.height=height
-        self.name=name
         self.color=color
-rect1=Rectangle("rectangle",100,3141)
-print(f"{rect1.name} {rect1.width}x{rect1.height} {rect1.color}")
-rect2=Rectangle("sqwar3e")
-print(f"{rect2.name} {rect2.width}x{rect2.height} {rect2.color}")
-rectan=[]
-colors=['red','orange','green','blue','yellow', 'purple']
+        self.name=name
+        Rectangle.count+=1
+    def inform_count():
+        print(f'Instancescreated: {Rectangle.count}')
+    def introduce(self):
+        print(f'{
+self.name
+}{self.color} {self.width}x{self.height}')
+    def calculate_area(self):
+        print(f'The area is {self.width*self.height}')
+
+'''
+        self.width=int(input("ievadiet platumu: "))
+        self.height=int(input("ievadiet garumu: "))
+    '''   
+rect1=Rectangle('Saquare',100,100,'Orange')
+print(f'{
+rect1.name
+} {rect1.color} {rect1.width}x{rect1.height}')
+rect1.calculate_area()
+rect2=Rectangle('Rect')
+print(f'{
+rect2.name
+} {rect2.color} {rect2.width}x{rect2.height}')
+rect2.calculate_area()
+
+rectangles=[]
+colors=['red','orange','green','blue', 'yellow']
 for i in range(100):
-        rectan.append(Rectangle(f"Name{i+1}",randint(1,500), randint(1,500) ,
-      choice(colors)))
-for rect in rectan:
-    print(f"{rect.name} {rect.width}x{rect.height} {rect.color}") 
+    rectangles.append(Rectangle(f'Name{i+1}', random.randint(1,500), random.randint(1,500), random.choice(colors) ))
+
+for rect in rectangles:
+    print(f'{
+rect.name
+} {rect.color} {rect.width}x{rect.height}')
+   # print(rect)
+print(Rectangle.count)
+Rectangle.inform_count() 
